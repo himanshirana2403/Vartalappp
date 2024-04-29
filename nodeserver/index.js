4967d66cbfd94ef5620303ad8316f3053d16cddb
Himanshi Rana <139847752+himanshirana2403@users.noreply.github.com>
2024-04-30 00:59:30 +0530

    Update index.js

--- nodeserver/index.js
+++ nodeserver/index.js
@@ -1,4 +1,4 @@
-const io = require('socket.io')({
+/*const io = require('socket.io')({
     cors: {
       origin: "*",
       methods: ["GET", "POST"]
@@ -34,4 +34,35 @@ const io = require('socket.io')({
 
 
   io.listen(8000);
+  */
+  const io = require('socket.io')({
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
+  
+  const users = {};
+  
+  io.on('connection', socket => {
+    socket.on('new-user-joined', name => {
+      console.log("New user joined:", name)
+      users[socket.id] = name;
+      socket.broadcast.emit('user-joined', name);
+    });
+  
+    socket.on('send', message => {
+      socket.broadcast.emit('receive', { message: message, name: users[socket.id] });
+    });
+  
+    socket.on('disconnect', () => {
+      socket.broadcast.emit('left', users[socket.id]);
+      delete users[socket.id];
+    });
+  });
+  
+  const PORT = process.env.PORT || 8000; // Use environment port or fallback to 8000
+  io.listen(PORT, '0.0.0.0', () => {
+    console.log(`Socket.IO server is running on port ${PORT}`);
+  });
   
