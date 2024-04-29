053aaf90ec3a774da7eb70a6ef751e6aae99e313
Himanshi Rana <139847752+himanshirana2403@users.noreply.github.com>
2024-04-30 01:04:04 +0530

    Update client.js

--- client.js
+++ client.js
@@ -1,9 +1,8 @@
 //const { name } = require("ejs");
 
-//const socket=io('http://localhost:8000');
+const socket=io('http://localhost:8000');
 
-//const socket=io('http://localhost:8000');
-const socket = io('http://vartalappp.vercel.app.com:8000');
+
 const form=document.getElementById('send-container');
 const messageInput=document.getElementById('messageInp');
 const messageContainer=document.querySelector('.main');
